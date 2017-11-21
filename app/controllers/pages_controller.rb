820ca4827f7303aa6df509f19c3b7583eeee0763
opstech23 <bbody8215@gmail.com>
2017-11-22 04:30:48 +0800

    configured bootstrap and changed links on navbar

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+  #controller action pages#index for /pages/index view and uri pattern respectively
+  def index
+  end
+
+  #controller action pages#home for /pages/home view and uri pattern respectively
+  def home
+  end
+
+  #controller action pages#profile for /pages/profile view and uri pattern respectively
+  def profile
+  end
+
+  #controller action pages#explore for /pages/explore view and uri pattern respectively
+  def explore
+  end
+end
